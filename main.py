71fcdf7bb9f4d5358b57df77686d18ca789d1a70
amy-maria <122695916+amy-maria@users.noreply.github.com>
2025-04-03 18:33:35 -0400

    final commit

--- main.py
+++ main.py
@@ -2,19 +2,25 @@ import sys
 
 from random import randint
 
-random_number = randint(int(sys.argv[1]), int(sys.argv[2]))
+# generate number 1-10
+random_number = randint(1, 10)
+print(random_number)
+
+
+# input from user
 
 while True:
     try:
-        number = int(
-            input(
-                "Please choose a number that falls between those two you just chose: "
-            )
-        )
-        if number >= int(sys.argv[1]) and number <= int(sys.argv[2]):
-            if number == random_number:
-                print("You're a genius!")
+        guess = int(input("Guess a number 1-10:  "))
+        # check input is a number
+        # check if number is correct guess or guess again
+        if int(guess) > 0 and int(guess) < 11:
+            if guess == random_number:
+                print("You're a genius! You guessed correctly")
                 break
+        else:
+            print("Number out of Range. Enter a number between 1-10.")
+            continue
     except ValueError:
         print("Please enter a number")
         continue
